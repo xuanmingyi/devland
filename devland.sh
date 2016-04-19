3c228dbe4da3675f48130a939144ca4f65ad60b3
xuanmingyi <xuanmingyi@gmail.com>
2016-04-20 01:56:16 +0800

    add drm install

--- devland.sh
+++ devland.sh
@@ -18,6 +18,7 @@ sudo apt-get upgrade -y
 sudo apt-get install -y git pkg-config autoconf libtool
 sudo apt-get install -y libffi-dev expat libexpat1-dev  libxml2-dev
 sudo apt-get install -y bison flex python python-pip python-dev
+sudo apt-get install -y x11proto-gl-dev libdrm-dev libpthread-stubs0-dev libpciaccess-dev
 
 sudo pip install  mako
 
@@ -35,4 +36,13 @@ cd $TOP
 git clone ~/packages/wayland-protocols
 cd wayland-protocols
 ./autogen.sh --prefix=/usr
+sudo make install
+
+
+# drm
+cd $TOP
+git clone ~/packages/drm
+cd drm
+./autogen.sh  --disable-intel --disable-radeon --disable-amdgpu --disable-nouveau --enable-vmwgfx --prefix=/usr
+make 
 sudo make install
