a1750205c8ff1ee6db783834da05565229b16c5d
xuanmingyi <xuanmingyi@gmail.com>
2016-04-19 22:08:35 +0800

    add wayland compile

--- devland.sh
+++ devland.sh
@@ -15,30 +15,12 @@ fi
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-if [ -z $WLD ];then
-cat >> ~/.bashrc <<EOF
-export WLD=$HOME/install   # change this to another location if you prefer
-export LD_LIBRARY_PATH=$WLD/lib
-export PKG_CONFIG_PATH=$WLD/lib/pkgconfig/:$WLD/share/pkgconfig/
-export PATH=$WLD/bin:$PATH
-export ACLOCAL_PATH=$WLD/share/aclocal
-export ACLOCAL="aclocal -I $ACLOCAL_PATH"
-EOF
-export WLD=$HOME/install   # change this to another location if you prefer
-export LD_LIBRARY_PATH=$WLD/lib
-export PKG_CONFIG_PATH=$WLD/lib/pkgconfig/:$WLD/share/pkgconfig/
-export PATH=$WLD/bin:$PATH
-export ACLOCAL_PATH=$WLD/share/aclocal
-export ACLOCAL="aclocal -I $ACLOCAL_PATH"
-mkdir -p $WLD/share/aclocal # needed by autotools
-fi
-
 sudo apt-get install -y git pkg-config autoconf libtool
-
+sudo apt-get install -y libffi-dev expat libexpat1-dev  libxml2-dev
 
 # install wayland
 
-git clone git://anongit.freedesktop.org/wayland/wayland
+git clone ~/packages/wayland.git
 cd wayland
-./autogen.sh --prefix=$WLD --disable-documentation
+./autogen.sh --prefix=/usr --disable-documentation
 make
